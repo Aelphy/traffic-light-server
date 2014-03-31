cd9c6dd183949d11adf698fe8320b1d65537775d
Aelphy <miha-usv@yandex.ru>
2014-03-31 13:23:32 +0600

    feature(traffic-light): Добавлена частичная авторизация

--- application.rb
+++ application.rb
@@ -9,55 +9,71 @@ require 'ox'
 require File.join(File.dirname(__FILE__), 'environment')
 require File.join(File.dirname(__FILE__), 'lib', 'traffic_light_mode')
 require File.join(File.dirname(__FILE__), 'lib', 'traffic_light_branch')
-
 set :bind, '0.0.0.0'
 
-set :views, "#{File.dirname(__FILE__)}/views"
+URL = 'http://ci.dev.apress.ru/XmlStatusReport.aspx'
 
-class Public < Sinatra::Base
-  get '/' do
-    haml :root
-  end
+configure do
+  set :views, "#{File.dirname(__FILE__)}/views"
+end
 
-  get '/mode' do
-    TrafficLightMode.get
-  end
+error do
+  e = request.env['sinatra.error']
+  Kernel.puts e.backtrace.join('\n')
+  'Application error'
+end
 
-  get '/branch' do
-    TrafficLightBranch.get
+def authorized?
+  @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+  @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['t','a']
+end
+
+def protected!
+  unless authorized?
+    response['WWW-Authenticate'] = %(Basic realm='Restricted Area')
+    throw(:halt, [401, 'Oops... we need your login name & password\n'])
   end
 end
 
-class Protected < Sinatra::Base
-  URL = 'http://ci.dev.apress.ru/XmlStatusReport.aspx'
-
-  use Rack::Auth::Basic, 'Restricted Area' do |username, password|
-    username == 'traffic-light-administrator' and password == 'traffic-light'
-  end
-
-  get '/standart' do
-    @mode = 'Стандартный'
-    TrafficLightMode.set @mode
-    haml :mode
-  end
-
-  get '/new_year' do
-    @mode = 'Новый Год'
-    TrafficLightMode.set @mode
-    haml :mode
-  end
-
-  post '/choose' do
-    @branch = params[:branch]
-
-    @avaliable_branches = Ox.parse(open(URL).read).locate('Project').map { |node| node[:name] }
-
-    unless @avaliable_branches.include? @branch
-      return haml :branch_error
-    end
-
-    TrafficLightBranch.set @branch
-    haml :branch
-  end
+get '/' do
+  haml :root
 end
 
+get '/mode' do
+  TrafficLightMode.get
+end
+
+get '/branch' do
+  TrafficLightBranch.get
+end
+
+get '/standart' do
+  protected!
+
+  @mode = 'Стандартный'
+  TrafficLightMode.set @mode
+  haml :mode
+end
+
+get '/new_year' do
+  protected!
+
+  @mode = 'Новый Год'
+  TrafficLightMode.set @mode
+  haml :mode
+end
+
+post '/choose' do
+  protected!
+
+  @branch = params[:branch]
+
+  @avaliable_branches = Ox.parse(open(URL).read).locate('Project').map { |node| node[:name] }
+
+  unless @avaliable_branches.include? @branch
+    return haml :branch_error
+  end
+
+  TrafficLightBranch.set @branch
+  haml :branch
+end
--- config.ru
+++ config.ru
@@ -1,16 +1,12 @@
 require File.join(File.dirname(__FILE__), 'application')
 
+set :run, false
+
+set :environment, :production
+
 FileUtils.mkdir_p 'log' unless File.exists?('log')
-log = File.new("log/sinatra.log", "a+")
+log = File.new('log/sinatra.log', 'a+')
 $stdout.reopen(log)
 $stderr.reopen(log)
 
-run Rack::URLMap.new({
-  '/' => Public,
-  '/standart' => Protected,
-  '/new_year' => Protected,
-  '/choose' => Protected,
-  '/mode' => Public,
-  '/branch' => Public
-})
-
+run Sinatra::Application
